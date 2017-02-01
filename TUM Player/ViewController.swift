2df26cdc7113127cd0f52d19fe9fc6a7cdf418da
Finn Gaida <f@fga.pw>
2017-02-01 22:27:42 +0100

    Initial Commit

---
+++ TUM Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TUM Player
+//
+//  Created by Finn Gaida on 01/02/2017.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
